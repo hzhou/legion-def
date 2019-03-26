4fb82591190540ee670c370f45e364f1626555ad
Hui Zhou <hzhou321@anl.gov>
2019-03-26 11:25:04 -0500

    Add incoming message handler threads

--- templates/activemsg_impl_mpi.cc
+++ templates/activemsg_impl_mpi.cc
@@ -1,3 +1,4 @@
+/*---- polling threads ---------------------*/
 class AM_Manager {
 public:
     AM_Manager(){
@@ -7,18 +8,16 @@ public:
     }
     ~AM_Manager(void){}
     void init_corereservation(Realm::CoreReservationSet& crs){
-        core_rsrv = new Realm::CoreReservation("AM workers", crs, Realm::CoreReservationParameters());
+        $call _core_rsrv, "AM workers"
     }
     void start_threads(){
-        p_thread = Realm::Thread::create_kernel_thread<AM_Manager, &AM_Manager::thread_loop>(this, Realm::ThreadLaunchParameters(), *core_rsrv);
+        $call _start_polling_threads
     }
     void thread_loop(void){
-        $call mpi_thread_loop
+        $call _polling_thread_loop
     }
     void stop_threads(){
-        shutdown_flag = true;
-        p_thread->join();
-        delete p_thread;
+        $call _shutdown_polling_threads
     }
 protected:
     $call hsl_cond_decl
@@ -30,7 +29,57 @@ protected:
 
 AM_Manager g_am_manager;
 
-/*---------------------------------------------*/
+/*---- handler threads ---------------------*/
+struct I_Msg {
+    I_Msg(int _src, bool _need_free, unsigned int *a, char *b){
+        src = _src;
+        need_free = _need_free;
+        buf_recv = a;
+        buf_payload = b;
+        next = NULL;
+    }
+
+    int src;
+    bool need_free;
+    unsigned int *buf_recv;
+    char *buf_payload;
+    struct I_Msg* next;
+};
+
+class Incoming_Manager {
+public:
+    Incoming_Manager(){
+        head=NULL;
+        num_msgs = 0;
+        shutdown_flag = false;
+    }
+    ~Incoming_Manager(void){}
+
+    void start_threads(Realm::CoreReservationSet& crs, int count, size_t stack_size){
+        $call _start_handler_threads
+    }
+    void thread_loop(void){
+        $call _handler_thread_loop
+    }
+    void shutdown(){
+        $call _shutdown_handler_threads
+    }
+    void enqueue_incoming(int src, bool need_free, unsigned int *buf_recv, char *buf_payload){
+        $call _enqueue_incoming
+    }
+protected:
+    $call hsl_cond_decl
+    Realm::CoreReservation *core_rsrv;
+    bool shutdown_flag;
+
+    struct I_Msg *head, *tail;
+    int num_msgs;
+    std::vector<Realm::Thread *> handler_threads;
+};
+
+Incoming_Manager g_incoming_manager;
+
+/*---- interface functions ----------------*/
 void enqueue_message(NodeID target, int msgid,
                     const void *args, size_t arg_size,
                     const void *payload, size_t payload_size,
@@ -87,6 +136,7 @@ NodeID get_message_source(token_t token)
 void enqueue_incoming(NodeID sender, IncomingMessage *msg)
 {
     $call mpi_enqueue_incoming
+    // assert(0);
 }
 
 bool adjust_long_msgsize(NodeID source, void *&ptr, size_t &buffer_size,
@@ -119,7 +169,7 @@ void start_polling_threads(int count)
     $call mpi_start_polling_threads
 }
 
-void start_handler_threads(int count, Realm::CoreReservationSet&, size_t)
+void start_handler_threads(int count, Realm::CoreReservationSet& crs, size_t stack)
 {
     $call mpi_start_handler_threads
 }
